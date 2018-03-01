a790871612c345866e7cf9d568c621753b8ca71d
Des Barrett <des.barrett@gmail.com>
2018-03-01 18:13:01 +0000

    added es5 dir and uglify working

--- es5/js/main.js
+++ es5/js/main.js
@@ -2,10 +2,9 @@
 
 var test = [1, 2, 3, 4, 5, 6];
 
-
-const temp = 100;
-const test2 = 90;
-const freeMonkey = 2000;
+var temp = 100;
+var test2 = 90;
+var freeMonkey = 2000;
 
 var double = test.map(function(num) {
   return num * 2;
